7209be2f70f496bbe22a6212acc9a6aab80d2b0c
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-20 21:24:15 +0530

    Added Projects to App

--- src/data/app.ts
+++ src/data/app.ts
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import Home from '../home/Home'
 import Skills from '../skills/Skills'
+import Projects from '../projects/Projects'
 
 export const appData: Map<string, React.FunctionComponent | null> = new Map([
     ["Profile", Home],
     ["Skills", Skills],
-    ["Projects", null],
+    ["Projects", Projects],
     ["Education", null]
 ])
