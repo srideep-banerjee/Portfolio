06ec3c887f7f1d380379f978c71348b8c3f95c8c
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-20 21:08:15 +0530

    Added Projects starter files

---
+++ src/data/projects.ts
@@ -0,0 +1,15 @@
+export type Project = {
+    name: string,
+    description: string,
+    skills: Array<string>
+}
+
+export const categoryData = [
+    "All",
+    "Android",
+    "Flutter",
+    "React-Native",
+    "Backend",
+    "Frontend",
+    "DBMS",
+]
---
+++ src/projects/Projects.tsx
@@ -0,0 +1,8 @@
+import "../styles/common.css"
+import "../styles/Projects.css"
+
+export default function Projects() {
+    <div className="projects-container appbar-adjusted-height">
+        
+    </div>
+}
