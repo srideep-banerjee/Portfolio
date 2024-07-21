b1664197edb63fbfe6617de90b4aac78fbb2f9f5
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-21 10:19:46 +0530

    Added CategoryDisplay to Projects

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+import CategoryDisplay from "../common/CategoryDispaly"
 import "../styles/common.css"
 import "../styles/Projects.css"
+import { projectsCategoryData } from "../data/projects"
 
 export default function Projects() {
+    const [category, setCategory] = useState(0)
+
     return (
         <div className="projects-container appbar-adjusted-height">
-            
+            <CategoryDisplay
+                data={projectsCategoryData}
+                selectedIndex={category}
+                onSelectionChange={setCategory}/>
+            <hr />
+            <ul id='skill-list'>
+                
+            </ul>
         </div>
     )
 }
