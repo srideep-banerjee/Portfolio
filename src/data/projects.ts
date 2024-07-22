bea92ef0679a8dc7e05048d8ea24935641f8a698
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-22 22:31:04 +0530

    Added temporary project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,9 @@
 export type Project = {
     name: string,
     description: string,
+    github?: string
+    apk?: string,
+    link?: string,
     skills: Array<string>
 }
 
@@ -12,4 +15,22 @@ export const projectsCategoryData = [
     "Backend",
     "Frontend",
     "DBMS",
+]
+
+export const projectsData: Array<Project> = [
+    {
+        name: "Notes-Synced",
+        description: `An Android / Web application built using flutter and firebase backend to perform CRUD operations on notes, synced across multiple devices`,
+        link: "https://notes-102da.web.app/",
+        skills: [
+            "Flutter",
+            "Firebase Firestore",
+            "Dart",
+            "Stream",
+            "Future",
+            "provider",
+            "sqflite",
+            "Indexed DB"
+        ]
+    }
 ]
--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import CategoryDisplay from "../common/CategoryDispaly"
 import "../styles/common.css"
 import "../styles/Projects.css"
-import { projectsCategoryData } from "../data/projects"
+import { Project, projectsCategoryData, projectsData } from "../data/projects"
 
 export default function Projects() {
     const [category, setCategory] = useState(0)
@@ -15,8 +15,20 @@ export default function Projects() {
                 onSelectionChange={setCategory}/>
             <hr />
             <ul id='project-list'>
-                
+                {
+                    projectsData.map((project)=>(<ProjectItem project={project} />))
+                }
             </ul>
         </div>
+    )
+}
+
+function ProjectItem({project}: {project: Project}) {
+    return (
+        <li className="project-item">
+            {project.name}
+            <pre className="project-item-description">{project.description}</pre>
+            {project.skills.join(", ")}
+        </li>
     )
 }
