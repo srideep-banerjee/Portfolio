a94eb6075578151f5c8c0065e067847012067167
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-27 21:34:28 +0530

    Modified projects tab

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import CategoryDisplay from "../common/CategoryDispaly"
 import "../styles/common.css"
 import "../styles/Projects.css"
+import LinkSvg from "../assets/link.svg?react"
 import { Project, projectsCategoryData, projectsData } from "../data/projects"
 
 export default function Projects() {
@@ -24,10 +25,41 @@ export default function Projects() {
 }
 
 function ProjectItem({project}: {project: Project}) {
+    const links = []
+    if (project.link != undefined) {
+        links.push((
+            <a className="project-link" href={project.link}>
+                <LinkSvg className="project-link-icon" />
+                Link
+            </a>
+        ))
+    }
+
+    if (project.apk != undefined) {
+        links.push((
+            <a className="project-link" href={project.apk}>
+                <LinkSvg className="project-link-icon" />
+                Apk
+            </a>
+        ))
+    }
+
+    if (project.github != undefined) {
+        links.push((
+            <a className="project-link" href={project.github}>
+                <LinkSvg className="project-link-icon" />
+                GitHub
+            </a>
+        ))
+    }
+    
     return (
         <li className="project-item">
-            {project.name}
-            <pre className="project-item-description">{project.description}</pre>
+            <span className="project-name">{project.name}</span>
+            <pre className="project-description">{project.description}</pre>
+            <section className="project-links-container">
+                {links}
+            </section>
             {project.skills.join(", ")}
         </li>
     )
